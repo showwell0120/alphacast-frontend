a6a07f9b28c17fd6120f2d61a67cd90418509c99
Yuri Tsai <showwell0120@gmail.com>
2023-05-29 00:29:14 +0800

    feat: add user dropdown

---
+++ src/components/user-dropdown/index.tsx
@@ -0,0 +1,58 @@
+import { MouseEvent, ReactNode, forwardRef } from 'react';
+import Dropdown from 'react-bootstrap/Dropdown';
+
+import { ReactComponent as DownIcon } from '../../assets/chevron-down.svg';
+import { useUserContext } from '../../contexts';
+import styles from './styles.module.scss';
+
+// The forwardRef is important!!
+// Dropdown needs access to the DOM node in order to position the Menu
+const CustomToggle = forwardRef<
+  HTMLDivElement,
+  {
+    children: ReactNode;
+    onClick: (e: MouseEvent<HTMLDivElement>) => void;
+  }
+>(({ children, onClick }, ref) => (
+  <div
+    className={styles['wrapper']}
+    ref={ref}
+    onClick={e => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  >
+    {children}
+    <DownIcon />
+  </div>
+));
+
+export function UserDropdown() {
+  const { spotifyProfile } = useUserContext();
+
+  if (!spotifyProfile) {
+    return null;
+  }
+
+  const handleNavigate = () => {
+    //
+  };
+
+  return (
+    <Dropdown>
+      <Dropdown.Toggle as={CustomToggle}>
+        <div>
+          <img
+            className={styles['avatar']}
+            src={spotifyProfile?.images?.[0].url}
+            alt={spotifyProfile.display_name}
+          />
+          <span>{spotifyProfile.display_name}</span>
+        </div>
+      </Dropdown.Toggle>
+      <Dropdown.Menu>
+        <Dropdown.Item onClick={handleNavigate}>查看我的個人資訊</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+}
