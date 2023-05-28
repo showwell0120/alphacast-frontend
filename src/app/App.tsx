5ab14d929a8d0d2acbfc82f05368adadc2144ca9
Yuri Tsai <showwell0120@gmail.com>
2023-05-28 15:37:25 +0800

    feat: setup react-query

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,8 +1,32 @@
+import {
+  MutationCache,
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { AxiosError } from 'axios';
 import { ToastContainer } from 'react-toastify';
+
+// Create a client
+const queryClient = new QueryClient({
+  mutationCache: new MutationCache({
+    onError: error => {
+      const response = error as AxiosError;
+      console.log(response);
+    },
+  }),
+  queryCache: new QueryCache({
+    onError: error => {
+      const response = error as AxiosError;
+      console.log(response.code);
+    },
+  }),
+});
 
 function App() {
   return (
     <>
+      <QueryClientProvider client={queryClient}></QueryClientProvider>
       <ToastContainer
         position="bottom-right"
         autoClose={5000}
